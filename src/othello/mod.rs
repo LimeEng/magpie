221bc6b075a1f33c0139d77e84b9859d407bcc23
Emil Englesson <englesson.emil@gmail.com>
2021-01-08 00:31:49 +0100

    Added a method to remove stones from the board
    Signed-off-by: Emil Englesson <englesson.emil@gmail.com>

--- src/othello/mod.rs
+++ src/othello/mod.rs
@@ -1,6 +1,6 @@
 /// Represents an Othello board and provides convenient methods to safely manipulate it.
 mod othello_board;
-// /// An enum that represents the two stone colors players can play with.
+/// An enum that represents the two stone colors players can play with.
 mod stone;
 
 pub use othello_board::OthelloBoard;
