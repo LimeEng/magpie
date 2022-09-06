b49c2b2dfdb7ee57e1d5865b7d3579970663f58f
Emil Englesson <englesson.emil@gmail.com>
2022-09-06 18:12:37 +0200

    Generate constants during compilation
    Previously the constants were specified in their own file, and any code
    that was needed to generate those constants were left as comments in
    case it was needed again. With this change all constants can now be
    generated during compilation instead, thus better conserving the code in
    question.

---
+++ build/gen_constants/common.rs
@@ -0,0 +1,58 @@
+pub const BLACK_START_POS: u64 = 0x00_00_00_08_10_00_00_00;
+pub const WHITE_START_POS: u64 = 0x00_00_00_10_08_00_00_00;
+
+// Bitboards representing their named rank
+pub const RANK_1: u64 = 0xff_00_00_00_00_00_00_00;
+pub const RANK_2: u64 = 0x00_ff_00_00_00_00_00_00;
+pub const RANK_3: u64 = 0x00_00_ff_00_00_00_00_00;
+pub const RANK_4: u64 = 0x00_00_00_ff_00_00_00_00;
+pub const RANK_5: u64 = 0x00_00_00_00_ff_00_00_00;
+pub const RANK_6: u64 = 0x00_00_00_00_00_ff_00_00;
+pub const RANK_7: u64 = 0x00_00_00_00_00_00_ff_00;
+pub const RANK_8: u64 = 0x00_00_00_00_00_00_00_ff;
+pub const RANKS: [u64; 8] = [
+    RANK_1, RANK_2, RANK_3, RANK_4, RANK_5, RANK_6, RANK_7, RANK_8,
+];
+
+// Bitboards representing their named file
+pub const FILE_A: u64 = 0x80_80_80_80_80_80_80_80;
+pub const FILE_B: u64 = 0x40_40_40_40_40_40_40_40;
+pub const FILE_C: u64 = 0x20_20_20_20_20_20_20_20;
+pub const FILE_D: u64 = 0x10_10_10_10_10_10_10_10;
+pub const FILE_E: u64 = 0x08_08_08_08_08_08_08_08;
+pub const FILE_F: u64 = 0x04_04_04_04_04_04_04_04;
+pub const FILE_G: u64 = 0x02_02_02_02_02_02_02_02;
+pub const FILE_H: u64 = 0x01_01_01_01_01_01_01_01;
+pub const FILES: [u64; 8] = [
+    FILE_A, FILE_B, FILE_C, FILE_D, FILE_E, FILE_F, FILE_G, FILE_H,
+];
+
+// Shifts for each direction, in the following order:
+// N, NE, E, SE, S, SW, W, NW.
+pub const SHIFT_DIRS: [i8; 8] = [-8, -7, 1, 9, 8, 7, -1, -9];
+
+// Shift masks for each direction, in the following order:
+// N, NE, E, SE, S, SW, W, NW.
+pub const SHIFT_MASKS: [u64; 8] = [
+    !RANK_1,
+    !(RANK_1 | FILE_H),
+    !FILE_H,
+    !(RANK_8 | FILE_H),
+    !RANK_8,
+    !(RANK_8 | FILE_A),
+    !FILE_A,
+    !(RANK_1 | FILE_A),
+];
+
+// Masks representing each position on the board starting with A1.
+#[rustfmt::skip]
+pub const POSITIONS: [u64; 64] = [
+    1 << 63, 1 << 62, 1 << 61, 1 << 60, 1 << 59, 1 << 58, 1 << 57, 1 << 56,
+    1 << 55, 1 << 54, 1 << 53, 1 << 52, 1 << 51, 1 << 50, 1 << 49, 1 << 48,
+    1 << 47, 1 << 46, 1 << 45, 1 << 44, 1 << 43, 1 << 42, 1 << 41, 1 << 40,
+    1 << 39, 1 << 38, 1 << 37, 1 << 36, 1 << 35, 1 << 34, 1 << 33, 1 << 32,
+    1 << 31, 1 << 30, 1 << 29, 1 << 28, 1 << 27, 1 << 26, 1 << 25, 1 << 24,
+    1 << 23, 1 << 22, 1 << 21, 1 << 20, 1 << 19, 1 << 18, 1 << 17, 1 << 16,
+    1 << 15, 1 << 14, 1 << 13, 1 << 12, 1 << 11, 1 << 10, 1 << 9 , 1 << 8 ,
+    1 << 7 , 1 << 6 , 1 << 5 , 1 << 4 , 1 << 3 , 1 << 2 , 1 << 1 , 1 << 0 ,
+];
---
+++ build/gen_constants/shift_rays.rs
@@ -0,0 +1,110 @@
+use crate::gen_constants::common::{FILE_A, FILE_H, POSITIONS, SHIFT_DIRS};
+
+pub fn generate() -> String {
+    let masks = vec![
+        u64::MAX,
+        !FILE_A,
+        !FILE_A,
+        !FILE_A,
+        u64::MAX,
+        !FILE_H,
+        !FILE_H,
+        !FILE_H,
+    ];
+    let mut result: Vec<Vec<u64>> = Vec::new();
+    for pos in u64::MAX.bits() {
+        let mut pos_result = Vec::new();
+        for (i, shift) in SHIFT_DIRS.iter().enumerate() {
+            let mut moves = 0;
+            let mut candidates = dir_shift(pos, *shift) & masks[i];
+            while candidates != 0 {
+                moves |= candidates;
+                candidates = dir_shift(candidates, *shift) & masks[i];
+            }
+            pos_result.push(moves);
+        }
+        result.push(pos_result)
+    }
+
+    let formatted_numbers: Vec<String> = result
+        .iter()
+        .map(|pos_rays| {
+            let hex_formatted: Vec<String> = pos_rays.iter().map(|ray| to_hex(*ray)).collect();
+            format!(
+                "    [ {}, {}, {}, {},\n      {}, {}, {}, {} ]",
+                hex_formatted.get(0).unwrap(),
+                hex_formatted.get(1).unwrap(),
+                hex_formatted.get(2).unwrap(),
+                hex_formatted.get(3).unwrap(),
+                hex_formatted.get(4).unwrap(),
+                hex_formatted.get(5).unwrap(),
+                hex_formatted.get(6).unwrap(),
+                hex_formatted.get(7).unwrap()
+            )
+        })
+        .collect();
+
+    let comment_lines = vec![
+        "For each position on the board starting with A1, shift rays are presented in",
+        "the following order: N, NE, E, SE, S, SW, W, NW.",
+        "Each shift ray excludes the aformentioned position and extends to the end of the board.",
+    ];
+
+    let comment = comment_lines
+        .iter()
+        .map(|line| format!("// {}", line))
+        .collect::<Vec<String>>()
+        .join("\n");
+
+    format!(
+        "{}\n#[rustfmt::skip]\npub const SHIFT_RAYS: [[u64; 8]; 64] = [\n{}\n];",
+        comment,
+        formatted_numbers.join(",\n")
+    )
+}
+
+fn to_hex(num: u64) -> String {
+    let mut hex = format!("{:#018x}", num);
+    for i in (4..23).step_by(3) {
+        hex.insert(i, '_');
+    }
+    hex
+}
+
+fn dir_shift(x: u64, shift: i8) -> u64 {
+    if shift > 0 {
+        x >> shift
+    } else {
+        x << -shift
+    }
+}
+
+pub trait HotBits: Sized {
+    type Iter: Iterator<Item = Self>;
+    fn hot_bits(&self) -> Self::Iter;
+}
+
+impl HotBits for u64 {
+    type Iter = Box<dyn Iterator<Item = u64>>;
+    fn hot_bits(&self) -> Self::Iter {
+        let this = *self;
+        let iter = POSITIONS.iter().map(move |m| m & this).filter(|m| *m != 0);
+
+        Box::new(iter)
+    }
+}
+
+pub trait Bits: Sized {
+    type Iter: Iterator<Item = Self>;
+    fn bits(&self) -> Self::Iter;
+}
+
+impl Bits for u64 {
+    type Iter = Box<dyn Iterator<Item = u64>>;
+    fn bits(&self) -> Self::Iter {
+        let this = *self;
+        let iter = POSITIONS.iter().map(move |m| m & this);
+
+        Box::new(iter)
+    }
+}
