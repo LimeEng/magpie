d2122edcc3679f7de664edd937a29c435f8da211
Emil Englesson <englesson.emil@gmail.com>
2023-02-15 15:49:48 +0100

    Added and updated tests

--- tests/bitboard.rs
+++ tests/bitboard.rs
@@ -4,7 +4,7 @@ use std::convert::TryFrom;
 
 mod common;
 
-use common::ShadowPosition;
+use common::{ShadowBitboard, ShadowPosition};
 
 #[quickcheck]
 fn bitboard_raw_matches(num: u64) {
@@ -18,7 +18,7 @@ fn position_equals_bitboard(position: ShadowPosition) {
 }
 
 #[quickcheck]
-fn bitboards_handles_bitwise(num1: u64, num2: u64) {
+fn bitboard_handles_bitwise(num1: u64, num2: u64) {
     let board1 = Bitboard::from(num1);
     let board2 = Bitboard::from(num2);
 
@@ -46,6 +46,69 @@ fn bitboards_handles_bitwise(num1: u64, num2: u64) {
 }
 
 #[test]
+fn position_rank_matches() {
+    let expected_order = vec![
+        0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3,
+        3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7,
+        7, 7, 7, 7,
+    ];
+    let all_equal = Bitboard::from(u64::MAX)
+        .hot_bits()
+        .map(Position::rank)
+        .zip(expected_order)
+        .all(|(a, b)| a == b);
+    assert!(all_equal);
+}
+
+#[test]
+fn position_file_matches() {
+    let expected_order = vec![
+        0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5,
+        6, 7, 0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3,
+        4, 5, 6, 7,
+    ];
+    let all_equal = Bitboard::from(u64::MAX)
+        .hot_bits()
+        .map(Position::file)
+        .zip(expected_order)
+        .all(|(a, b)| a == b);
+    assert!(all_equal);
+}
+
+#[test]
+fn position_notation_matches() {
+    let expected_order = vec![
+        "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1", "a2", "b2", "c2", "d2", "e2", "f2", "g2",
+        "h2", "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3", "a4", "b4", "c4", "d4", "e4", "f4",
+        "g4", "h4", "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5", "a6", "b6", "c6", "d6", "e6",
+        "f6", "g6", "h6", "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7", "a8", "b8", "c8", "d8",
+        "e8", "f8", "g8", "h8",
+    ];
+    let all_equal = Bitboard::from(u64::MAX)
+        .hot_bits()
+        .map(Position::to_notation)
+        .zip(expected_order)
+        .all(|(a, b)| a == b);
+    assert!(all_equal);
+}
+
+#[test]
+fn position_notation_identity() {
+    let full_notation = vec![
+        "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1", "a2", "b2", "c2", "d2", "e2", "f2", "g2",
+        "h2", "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3", "a4", "b4", "c4", "d4", "e4", "f4",
+        "g4", "h4", "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5", "a6", "b6", "c6", "d6", "e6",
+        "f6", "g6", "h6", "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7", "a8", "b8", "c8", "d8",
+        "e8", "f8", "g8", "h8",
+    ];
+
+    for n in &full_notation {
+        let identity = Position::try_from(*n).map(Position::to_notation).unwrap();
+        assert_eq!(*n, identity);
+    }
+}
+
+#[test]
 fn full_bitboard_bits_equal_hot_bits() {
     let v1 = Bitboard::from(u64::MAX).bits();
     let v2 = Bitboard::from(u64::MAX).hot_bits();
@@ -54,4 +117,25 @@ fn full_bitboard_bits_equal_hot_bits() {
     let v2: Vec<u64> = v2.map(Position::raw).collect();
 
     assert_eq!(v1, v2);
+}
+
+#[quickcheck]
+fn squares_bit_count(rand_bitboard: ShadowBitboard) {
+    let rand_bitboard = Bitboard::from(rand_bitboard);
+    let bit_at = |index: usize| rand_bitboard & (1 << index);
+    let success = rand_bitboard
+        .bits()
+        .enumerate()
+        .all(|(index, pos)| bit_at(63 - index) == pos);
+
+    assert!(success);
+}
+
+#[quickcheck]
+fn stones_bit_count(rand_bitboard: ShadowBitboard) {
+    let rand_bitboard = Bitboard::from(rand_bitboard);
+    let expected = rand_bitboard.count_set();
+    let result = rand_bitboard.hot_bits().filter(|pos| *pos != 0).count();
+
+    assert_eq!(expected as usize, result);
 }
--- tests/othello_board.rs
+++ tests/othello_board.rs
@@ -1,5 +1,10 @@
-use magpie::othello::{Board, Stone};
+use magpie::othello::{Bitboard, Board, Position, Stone};
+use quickcheck_macros::quickcheck;
 use std::convert::TryInto;
+
+mod common;
+
+use common::ShadowBoard;
 
 #[test]
 fn legal_move_check_one_valid() {
@@ -31,4 +36,43 @@ fn board_no_legal_moves() -> Board {
     let white_pos = (board.bits_for(Stone::White) | board.bits_for(Stone::Black)).into();
 
     (black_pos, white_pos).try_into().unwrap()
+}
+
+#[quickcheck]
+fn bits_should_be_consistent(board: ShadowBoard) {
+    // Check that black and white stones do not overlap with each other or the
+    // empty set
+    let board = Board::try_from(board).unwrap();
+
+    let black = board.bits_for(Stone::Black);
+    let white = board.bits_for(Stone::White);
+    let empty = board.empty_squares();
+
+    assert!(black & white == 0);
+    assert!((black | white) & empty == 0);
+}
+
+#[quickcheck]
+fn stone_at_consistency(board: ShadowBoard) {
+    // Check that stone_at returns the correct stones
+    let board = Board::try_from(board).unwrap();
+
+    let black = board.bits_for(Stone::Black);
+    let white = board.bits_for(Stone::White);
+    let empty = board.empty_squares();
+
+    let success = Bitboard::from(u64::MAX)
+        .bits()
+        .filter_map(|pos| Position::try_from(pos).ok())
+        .all(|pos| {
+            board
+                .stone_at(pos)
+                .map(|stone| match stone {
+                    Stone::Black => pos & black != 0,
+                    Stone::White => pos & white != 0,
+                })
+                .unwrap_or_else(|| pos & empty != 0)
+        });
+
+    assert!(success);
 }
